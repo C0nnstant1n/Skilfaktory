e814b4f17f4e1369e2fee2a987d018e37cdd8c1c
C0nstant1n <60222020+C0nnstant1n@users.noreply.github.com>
2022-12-31 17:34:15 +0700

    Add files via upload
    Добавлены упражнения из модуля 3.6

---
+++ exercise_3.6/3.6.9.py
@@ -0,0 +1,16 @@
+#1
+employee = 5
+#2
+i = 1
+#3
+while i < employee:
+#4
+    if i > 1:
+#5
+        print('There are', i, 'people in the department') # В отделе i человек
+#6
+    if i == 1:
+#7
+        print('There are', i, 'people in the department') # В отделе i человек
+#8
+    i += 1
