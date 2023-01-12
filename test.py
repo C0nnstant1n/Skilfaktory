8339ec9a0eef3039d3741304c8a4e3ed707225ef
C0nstant1n <connstantino@mail.ru>
2023-01-13 00:37:01 +0700

    Пытаюсь собрать все воедино

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-def shadow_ship(x):
-    shadow = []
-    mask = (-1, -1), (-1, 0), (-1, 1), (0, -1),\
-        (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)
-    for j in x:
-        for i in mask:
-            c = map(sum, zip(i, j))
-            shadow.append(tuple(c))
-
-    shadow_set = set(shadow)
-    return shadow_set
+import random as rnd
 
 
-print(shadow_ship(([3, 1], [2, 1])))
+a = (1, 2)
+print(a[0] + 1)
