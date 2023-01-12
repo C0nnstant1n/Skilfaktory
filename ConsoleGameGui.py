320283212ca00ec5e17a2a55339fa15de2d5d3fc
C0nstant1n <connstantino@mail.ru>
2023-01-13 00:37:01 +0700

    Пытаюсь собрать все воедино

--- ConsoleGameGui.py
+++ ConsoleGameGui.py
@@ -1,3 +1,4 @@
+import random as rnd
 h, w = 6, 6
 
 
@@ -11,15 +12,24 @@ class Coordinates:
     def get_coordinates(self):
         return self.coordinates
 
+    def rand_coordinates(self, len, line):
+        self.len = len
+        self.line = line
+        if line:
+            self.set_coordinates((rnd.randint(1, w), rnd.randint(1, (h - self.len))))
+        else:
+            self.set_coordinates((rnd.randint(1, (w - self.len)), rnd.randint(1, h)))
+        return self.get_coordinates()
+
 
 class GameDesk:
-    def __init__(self, w=6, h=6, pos=None, ch="#"):
+    def __init__(self, w=6, h=6, pos=None, ch="# |"):
         self.width: int = w
         self.height: int = h
         self.pos = pos
         self.ch = ch
         self.game_desk = []
-        self.game_desk = [['-'] * self.width for _ in range(self.height)]
+        self.game_desk = [["- |"] * self.width for _ in range(self.height)]
 
     def get_game_desk(self):
         return self.game_desk
@@ -28,83 +38,201 @@ class GameDesk:
         for i in pos:
             self.game_desk[i[1]-1][i[0]-1] = ch
 
-
     def print_game_desk(self):
         s = []
         for i in range(self.width):
             s.append(i + 1)
-        print(" ", *s)
+            s.append("|")
+        print(" ", "|", *s)
         for i in range(self.height):
-            print((i + 1), *GameDesk.get_game_desk(self)[i])
+            print("---------------------------")
+            print((i + 1), "|",  *GameDesk.get_game_desk(self)[i])
 
 
 class Ship:
-    def __init__(self, pos, len_ship=0, line_ship=0):
-        self.x = 0
-        self.y = 0
-        self.set_pos(pos)
+    def __init__(self, pos=None, len_ship=0, line_ship=0):
+        self.pos = pos
         self.len_ship = len_ship
         self.ship_position = []
         self.line_ship = line_ship
         self.shadow = []
+        self.ships = []
 
     def set_pos(self, pos):
-        self.x = pos[0]
-        self.y = pos[1]
+        self.pos = pos
+        return self.pos
 
-    def ship_coordinates(self):
-        self.ship_position = [(self.x, self.y)]
+    def set_ship_coordinates(self):
+        self.ship_position.append(self.pos)
         if self.len_ship == 0:
             return self.ship_position
         elif self.line_ship == 0:
             for i in range(self.len_ship - 1):
-                self.ship_position.append(((self.x + 1 + i), self.y))
+                self.ship_position.append((self.pos[0] + 1 + i, self.pos[1]))
         elif self.line_ship == 1:
             for i in range(self.len_ship - 1):
-                self.ship_position.append((self.x, (self.y + 1 + i)))
+                self.ship_position.append((self.pos[0], (self.pos[1] + 1 + i)))
         return self.ship_position
 
     def shadow_ship(self):
-        mask = (-1, -1), (-1, 0), (-1, 1), (0, -1), \
-            (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)
-        for j in self.ship_coordinates():
+        mask = (-1, 0), (-1, 1), (0, -1), \
+            (0, 0), (0, 1), (1, 0)
+        for j in self.get_ship_coordinates():
             for i in mask:
                 c = map(sum, zip(i, j))
                 self.shadow.append(tuple(c))
         shadow_get = set(self.shadow)
         return shadow_get
 
+    def get_ship_coordinates(self):
+        return self.ship_position
 
-def input_coor(w, h):                               # Ввод координат и проверка ввода
-    x_y_str = input("Введите координаты поля через пробел (Y X)")
-    x_y = x_y_str.split()
-    list_coordinates = []
-    if len(x_y) == 2:
-        for i in x_y:
-            if not i.isdigit():                     # если строка содержит что то кроме цифр повторяем ввод
-                print("Введите только цифры")
-                return input_coor(w, h)
-            else:
-                if (0 < int(i) < h) and (0 < int(i) < w):
-                    list_coordinates.append(int(i))
+    def gen_ship(self, deck):
+        rand = Coordinates()
+        line = rnd.randint(0, 1)
+        ship = Ship(rand.rand_coordinates(deck, line), deck, line)
+        ship.set_ship_coordinates()
+        return ship
+
+    def gen_ships(self):
+        ship_3d = self.gen_ship(3)
+        shadows = []
+        self.ships.append(ship_3d)
+        shadows += ship_3d.shadow_ship()
+
+        ship_2d = self.gen_ship(2)
+        for i in range(2):
+            pos = set(ship_2d.get_ship_coordinates())
+            while pos.intersection(shadows):
+                ship_2d = self.gen_ship(2)
+                pos = set(ship_2d.get_ship_coordinates())
+            self.ships.append(ship_2d)
+            shadows += ship_2d.shadow_ship()
+
+        ship_1d = self.gen_ship(1)
+        for i in range(4):
+            pos = set(ship_1d.get_ship_coordinates())
+            while pos.intersection(shadows):
+                ship_1d = self.gen_ship(1)
+                pos = set(ship_1d.get_ship_coordinates())
+            self.ships.append(ship_1d)
+            shadows += ship_1d.shadow_ship()
+
+
+class GameLogic:
+    def __init__(self, w=6, h=6, ships=[], coordinates=[]):
+        self.width = w
+        self.height = h
+        self.ships = ships
+        # self.coordinates = coordinates
+        self.hit = False
+
+    # Нахождение попадания
+    def hit_ship(self, ships, coordinates):
+        targerts = []
+        for i in ships:
+            targerts += i.ship_position
+        if coordinates in targerts:
+            print("Попадание")
+            self.hit = True
+            return self.hit
+        else:
+            print("Промах")
+            return self.hit
+
+    # Проверка уничтожкния корабля
+    # Проверка количества оставшихся кораблей
+
+
+class GameEvent:
+    def __init__(self):
+        #self.player = player
+        #self.comp = comp
+        self.coordinates = Coordinates()
+
+        # ход игрока
+    def player_step(self):
+
+        x_y_str = input("Введите координаты поля через пробел (Y X)")
+        x_y = x_y_str.split()
+        self.coordinates = []
+        if len(x_y) == 2:
+            for i in x_y:
+                if not i.isdigit():  # если строка содержит что то кроме цифр повторяем ввод
+                    print("Введите только цифры")
+                    return self.player_step()
                 else:
-                    print("Координаты за пределами поля")
-                    return input_coor(w, h)
-    else:
-        return input_coor(w, h)
-    return list_coordinates
+                    if (0 < int(i) < h) and (0 < int(i) < w):
+                        self.coordinates.append(int(i))
+                    else:
+                        print("Координаты за пределами поля")
+                        return self.player_step()
+        else:
+            return self.player_step()
+        return self.coordinates
+
+        # ход компьютера
+    def comp_step(self):
+        self.coordinates.rand_coordinates(1, 0)
+        return self.coordinates
 
 
+player_desk = GameDesk()
+player = Ship()
+player.gen_ships()
+comp_desk = GameDesk()
+comp_virt_desk = GameDesk()
+comp = Ship()
+comp.gen_ships()
+event_player = GameEvent()
+logic_player = GameLogic()
+event_comp = GameEvent()
+logic_comp = GameLogic()
 
-print("Поле игрока")
 
-game = GameDesk()
-ship_1 = Ship([4, 5], 3, 1)
-#ship_3 = Ship([4, 3], 2, 1)
-game.print_game_desk()
-try:
-    game.set_game_desk(ship_1.ship_coordinates(), "#")
-except IndexError as e:
-    print("Введите другие координаты")
+for ship in player.ships:
+    player_desk.set_game_desk(ship.get_ship_coordinates(), "# |")
+print("Поле Игрока")
+player_desk.print_game_desk()
+
+for ship in comp.ships:
+    comp_virt_desk.set_game_desk(ship.get_ship_coordinates(), "# |")
+print("\nПоле компьютера")
+comp_desk.print_game_desk()
+
+# Ход Игрока
+logic_player.hit_ship(comp.ships, event_player.player_step())
+if logic_player.hit:
+    comp_desk.set_game_desk([event_player.coordinates], "X |")
 else:
-    game.print_game_desk()
+    comp_desk.set_game_desk([event_player.coordinates], "T |")
+print("Поле Игрока")
+player_desk.print_game_desk()
+print("\nПоле компьютера")
+comp_desk.print_game_desk()
+
+# Ход Компа
+logic_comp.hit_ship(comp.ships, event_comp.comp_step())
+if logic_comp.hit:
+    player_desk.set_game_desk([event_comp.coordinates], "X |")
+else:
+    player_desk.set_game_desk([event_comp.coordinates], "T |")
+print("Поле Игрока")
+player_desk.print_game_desk()
+print("\nПоле компьютера")
+comp_desk.print_game_desk()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
