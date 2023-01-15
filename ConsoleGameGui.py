3859f177ee8b58792603e82956fbf457062bd63c
C0nstant1n <connstantino@mail.ru>
2023-01-15 22:21:26 +0700

    Допиливаем Морской бой

--- ConsoleGameGui.py
+++ ConsoleGameGui.py
@@ -1,4 +1,5 @@
 import random as rnd
+from os import system, name
 h, w = 6, 6
 
 
@@ -13,13 +14,13 @@ class Coordinates:
     def get_coordinates(self):
         return self.coordinates
 
-    def rand_coordinates(self, len, line):
-        self.len = len
-        self.line = line
+    def rand_coordinates(self, deck, line):
+        # self.deck = deck
+        # self.line = line
         if line:
-            self.set_coordinates((rnd.randint(1, w), rnd.randint(1, (h - self.len))))
+            self.set_coordinates((rnd.randint(1, w), rnd.randint(1, (h - deck))))
         else:
-            self.set_coordinates((rnd.randint(1, (w - self.len)), rnd.randint(1, h)))
+            self.set_coordinates((rnd.randint(1, (w - deck)), rnd.randint(1, h)))
         return self.get_coordinates()
 
     def create_possble_coordinates(self):
@@ -56,79 +57,79 @@ class GameDesk:
 
 
 class Ship:
-    def __init__(self, pos=None, len_ship=0, line_ship=0):
-        self.pos = pos
-        self.len_ship = len_ship
-        self.ship_position = []
-        self.line_ship = line_ship
-        self.shadow = []
-        self.ships = []
+    def __init__(self, point=None, deck=0, orientation=0):
+        self.point = point  # Стартовые координаты
+        self.deck = deck    # Количество палуб корабля
+        self.position = []  # Список координат корабля
+        self.orientation = orientation # Ориентация корабля
+        self.shadow = []    # Тень корабля (нужна чтобы отделить корабли друг от друга)
+        self.ships = []     # Список координат множества кораблей
+        self.lships = []
 
     def set_pos(self, pos):
-        self.pos = pos
-        return self.pos
+        self.point = self.point
 
-    def set_ship_coordinates(self):
-        self.ship_position.append(self.pos)
-        if self.len_ship == 0:
-            return self.ship_position
-        elif self.line_ship == 0:
-            for i in range(self.len_ship - 1):
-                self.ship_position.append((self.pos[0] + 1 + i, self.pos[1]))
-        elif self.line_ship == 1:
-            for i in range(self.len_ship - 1):
-                self.ship_position.append((self.pos[0], (self.pos[1] + 1 + i)))
-        return self.ship_position
+    def set_position(self):
+        self.position.append(self.point)
+        if self.deck == 0:
+            return self.position
+        elif self.orientation == 0:
+            for i in range(self.deck - 1):
+                self.position.append((self.point[0] + 1 + i, self.point[1]))
+        elif self.orientation == 1:
+            for i in range(self.deck - 1):
+                self.position.append((self.point[0], (self.point[1] + 1 + i)))
 
     def shadow_ship(self):                      # тень нужна чтобы корабли создавались на расстоянии от текушего корабля
         mask = (-1, 0), (-1, 1), (0, -1), \
             (0, 0), (0, 1), (1, 0)
-        for j in self.get_ship_coordinates():
+        for j in self.get_position():
             for i in mask:
                 c = map(sum, zip(i, j))
                 self.shadow.append(tuple(c))
         shadow_get = set(self.shadow)
         return shadow_get
 
-    def get_ship_coordinates(self):
-        return self.ship_position
+    def get_position(self):
+        return self.position
 
     def gen_ship(self, deck):
         rand = Coordinates()
         line = rnd.randint(0, 1)
         ship = Ship(rand.rand_coordinates(deck, line), deck, line)
-        ship.set_ship_coordinates()
+        ship.set_position()
         return ship
 
     def gen_ships(self):
         ship_3d = self.gen_ship(3)
         shadows = []
+
         self.ships.append(ship_3d)
         shadows += ship_3d.shadow_ship()
 
         ship_2d = self.gen_ship(2)
         for i in range(2):
-            pos = set(ship_2d.get_ship_coordinates())
+            pos = set(ship_2d.get_position())
             while pos.intersection(shadows):
                 ship_2d = self.gen_ship(2)
-                pos = set(ship_2d.get_ship_coordinates())
+                pos = set(ship_2d.get_position())
             self.ships.append(ship_2d)
             shadows += ship_2d.shadow_ship()
 
         ship_1d = self.gen_ship(1)
         for i in range(4):
-            pos = set(ship_1d.get_ship_coordinates())
+            pos = set(ship_1d.get_position())
             while pos.intersection(shadows):
                 ship_1d = self.gen_ship(1)
-                pos = set(ship_1d.get_ship_coordinates())
+                pos = set(ship_1d.get_position())
             self.ships.append(ship_1d)
             shadows += ship_1d.shadow_ship()
 
         for i in self.ships:
-            self.ship_position += i.ship_position
+            self.position += i.position
 
     def get_ships(self):
-        return self.ship_position
+        return self.position
 
 
 class GameLogic:
@@ -146,21 +147,31 @@ class GameLogic:
             return False
 
     # Проверка уничтожкния корабля
-    # def is_destroyed(self, ships, ):
+    def is_destroyed(self, user, coordinates):
+        n = 0
+        for i in user.ships:
+            if coordinates in i.position:
+                user.ships[n].position.remove(coordinates)
+            if not i.position:
+                user.ships[n].position.append(1)
+                return True
+            n += 1
+        return False
+
     # # Проверка количества оставшихся кораблей
 
 
 class GameEvent:
-    def __init__(self):
+    def __init__(self, player, comp):
         self.player = player
         self.comp = comp
         self.coordinates = Coordinates()
         self.coordinates.create_possble_coordinates()
         self.possible_coordinates = self.coordinates.possible_coordinates.copy()
+        self.logic = GameLogic()
 
         # ход игрока
     def player_step(self):
-
         x_y_str = input("Введите координаты поля через пробел (Y X)")
         x_y = x_y_str.split()
         self.coordinates = []
@@ -185,61 +196,73 @@ class GameEvent:
         return self.coordinates
 
     def player_turn(self):
-
-        if logic.hit_ship(comp.get_ships(), tuple(self.player_step())):
-            comp_desk.set_game_desk([self.coordinates], "X |")
-            print("HIT, player turn")
+        if self.logic.hit_ship(self.comp.get_ships(), tuple(self.player_step())):
+            self.comp.set_game_desk([self.coordinates], "X |")
+            self.game_pass()
+            if self.logic.is_destroyed(self.comp, tuple(self.coordinates)):
+                print("Корабль уничтожен, ход игрока")
+                player.destroyed_ship += 1
+            else:
+                print("Попадание, ход игрока")
             return True
         else:
-            comp_desk.set_game_desk([self.coordinates], "T |")
-            print("False, comp turn")
+            self.comp.set_game_desk([self.coordinates], "T |")
             self.game_pass()
-            _ = input("for turn press Enter")
+            print("Промах, ход компьютера")
+            _ = input("для продолжения нажмите Enter")
             return False
 
     def comp_turn(self):
         coordinates = self.comp_step()
-        if logic.hit_ship(player.get_ships(), coordinates):
-            player_desk.set_game_desk([coordinates], "X |")
-            print("HIT, comp turn")
-            _ = input("for turn press Enter")
-            return True
-        else:
-            player_desk.set_game_desk([coordinates], "T |")
-            print("False, player turn")
+        if self.logic.hit_ship(self.player.get_ships(), coordinates):
+            self.player.set_game_desk([coordinates], "X |")
             self.game_pass()
+            if self.logic.is_destroyed(self.comp, tuple(self.coordinates)):
+                print("Корабль уничтожен,  ход компьютера")
+                comp.destroyed_ship += 1
+                _ = input("для продолжения нажмите Enter")
+            else:
+                print("Попадание, ход компьютера")
+                _ = input("для продолжения нажмите Enter")
+                return True
+        else:
+            self.player.set_game_desk([coordinates], "T |")
+            self.game_pass()
+            print("Промах, ход игрока")
             return False
 
     def game_pass(self):
-        print("Player field")
-        player_desk.print_game_desk()
-        print("Comp field")
-        comp_desk.print_game_desk()
+        if name == 'nt':  # for windows
+            _ = system('cls')
+        else:  # for mac and linux(here, os.name is 'posix')
+            _ = system('clear')
+        print("Игра \"Морской бой\"")
+        print("-----------------------------------------")
+        print(f"Поле игрока, кораблей уничтожено - {comp.destroyed_ship}")
+        self.player.print_game_desk()
+        print(f"Поле компьютера, кораблей уничтожено - {player.destroyed_ship}")
+        self.comp.print_game_desk()
 
 
-player_desk = GameDesk()
-player = Ship()
-player.gen_ships()
+class User(GameDesk, Ship):
+    def __init__(self, w, h):
+        super().__init__(w, h)
+        self.destroyed_ship = 0
+        self.ships = []
+        self.position = []
+        Ship.gen_ships(self)
 
-player_ships = player.ships
-a = []
-for i in player_ships:
-    a.append(i.ship_position)
+        # self.ships = self.get_ships()
 
-print(len(a))
 
-# comp_desk = GameDesk()
-# comp = Ship()
-# comp.gen_ships()
-#
-# event = GameEvent()
-# logic = GameLogic()
-#
-# player_desk.set_game_desk(player.get_ships(), "# |")
-#
-# event.game_pass()
-# while True:
-#     while event.player_turn():
-#         event.game_pass()
-#     while event.comp_turn():
-#         event.game_pass()
+player = User(w, h)
+comp = User(w, h)
+event = GameEvent(player, comp)
+player.set_game_desk(player.get_position(), "# |")
+event.game_pass()
+
+while True:
+    while event.player_turn():
+        pass
+    while event.comp_turn():
+        pass
