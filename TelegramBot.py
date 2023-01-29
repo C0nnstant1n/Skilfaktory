72d447c6c21d911719272d35206ecc4a22e48d8d
C0nstant1n <connstantino@mail.ru>
2023-01-29 22:47:35 +0700

    Начинаем писать бота

--- TelegramBot.py
+++ TelegramBot.py
@@ -7,14 +7,21 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f"Привет, {message.chat.username}")
+    bot.send_message(message.chat.id, "Чтобы узнать обменный курс отправьте сообщение боту"
+                                      " в виде <имя валюты,"
+                                      " цену которой вы хотите узнать> <имя валюты,"
+                                      " в которой надо узнать цену первой валюты>"
+                                      " <количество первой валюты>\n Чтобы узнать список доступных валют"
+                                      " отправьте /exchange")
 
 
-@bot.message_handler(content_types=["text"])
-def function_name(message):
-    print(message.chat.id)
-    print(message.chat.username)
-    print(message.text)
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=["exchange"])
+def exchange_list(message):
+    exchange = {"доллар": "USD",
+                "Рубль": "RUB",
+                "евро": "EUR",
+                "биткоин": "BTC"}
+    bot.send_message(message.chat.id, ", ".join(exchange.keys()))
 
 
 @bot.message_handler(content_types=["photo"])
