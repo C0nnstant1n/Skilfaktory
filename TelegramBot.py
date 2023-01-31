15f80cf1cbc6bb0875f941920ead727d7e1556eb
C0nstant1n <connstantino@mail.ru>
2023-01-31 23:08:12 +0700

    Изменил получение списка валют с сайта, прикрутил кэш

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,7 +1,23 @@
+import requests
+import json
 import telebot
+import redis
 
 ***REMOVED***
 bot = telebot.TeleBot(TOKEN)
+red = redis.Redis("localhost", 6379)
+
+
+def update_currency():  # Обновляем данные по валютам с сайта
+    res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+    res = json.loads(res.content)
+    red.set("valutes", json.dumps(res))     # Записываем валюты в кэш
+
+
+def get_currency():     # Получаем данные по валютам из кэша
+    res = json.loads(red.get("valutes"))
+    valutes = res.get("Valute")
+    return valutes
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -17,11 +33,19 @@ def send_welcome(message):
 
 @bot.message_handler(commands=["exchange"])
 def exchange_list(message):
-    exchange = {"доллар": "USD",
-                "Рубль": "RUB",
-                "евро": "EUR",
-                "биткоин": "BTC"}
-    bot.send_message(message.chat.id, ", ".join(exchange.keys()))
+    currency_dict = {}
+    valutes = get_currency()
+    date = json.loads(red.get("valutes")).get("Date")
+
+    for i in valutes:
+        currency_dict[valutes[i]['CharCode']] = valutes[i]['Name']
+    bot.send_message(message.chat.id, "\n".join(currency_dict.values()))
+    bot.send_message(message.chat.id, f"Последнее обновление курсов\n{date[:10]}, {date[11:16]}\nвремя Московское")
+
+
+@bot.message_handler(content_type=['text', ])
+def convert(message: telebot.types.Message):
+    pass
 
 
 @bot.message_handler(content_types=["photo"])
--- test.py
+++ test.py
@@ -3,14 +3,20 @@ import json
 import redis
 red = redis.Redis("localhost", 6379)
 
-#res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+# res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+# res = json.loads(res.content)
+
+#
+# red.set("valutes", json.dumps(res))
 
 res = json.loads(red.get("valutes"))
-
 valutes = res.get("Valute")
+date = json.loads(red.get("valutes")).get("Date")
+print(f"Последнее обновление курсов  {date[:10]}, {date[11:16]}, время Московское")
 
-for i in valutes:
-    print(valutes[i]["CharCode"], valutes[i]["Name"], valutes[i]["Value"])
 
+# for i in valutes:
+#     print(valutes[i]["CharCode"], valutes[i]["Name"],
+#           round((valutes[i]["Value"] / valutes[i]["Nominal"]), 2))
 
 
