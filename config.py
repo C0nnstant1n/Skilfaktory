77f58bf95c2b2ec4be1a3191ac5239db599bae1e
C0nstant1n <connstantino@mail.ru>
2023-02-01 15:17:15 +0700

    Добавил функцию расчета стоимости валют

---
+++ config.py
@@ -0,0 +1,4 @@
+import redis
+
+TOKEN = "5951364977:AAHMdWKuawtB0NxLrzTVqz5AognxGvXa4ss"
+CACHE = redis.Redis("localhost", 6379)
---
+++ extensions.py
@@ -0,0 +1,47 @@
+import requests
+import json
+from config import CACHE
+
+
+class Currency:
+    def __init__(self):
+        self.resource = {}
+        self.update_currency()
+
+    def update_currency(self):  # Обновляем данные по валютам с сайта
+        self.resource = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        self.resource = json.loads(self.resource.content)
+        CACHE.set("valutes", json.dumps(self.resource))     # Записываем валюты в кэш
+
+    def get_currency(self):     # Получаем данные по валютам из кэша
+        self.resource = json.loads(CACHE.get("valutes"))
+        return self.resource.get("Valute")
+
+    def get_date(self):
+        return self.resource.get("Date")
+
+    def get_price(self, quote, base, amount):
+        amount = int(amount)
+        currency_dict = self.get_currency()  # Получаем словарь валют
+
+        available_currencies = {"Рубль": "RUB"}            # Инвертируем значение в ключ,
+        # так как словарь валют имеет вид "USD": "Доллар США"
+        # нам же нужно обратное
+        for i in currency_dict:
+            available_currencies[(currency_dict[i]["Name"])] = i
+
+        quote = available_currencies.get(quote)
+        base = available_currencies.get(base)
+
+        if quote == "RUB":
+            result = str(round((currency_dict[base]["Value"] / currency_dict[base]["Nominal"] * amount), 2))
+            return result
+        if base == "RUB":
+            result = str(round(((1 / currency_dict[quote]["Value"]) * amount), 2))
+            print(result)
+            return result
+
+        price_quote = (currency_dict[quote]["Value"] / currency_dict[quote]["Nominal"])  # Цена валюты в рублях
+        price_base = (currency_dict[base]["Value"] / currency_dict[base]["Nominal"])
+        result = round((price_quote / price_base) * amount, 2)
+        return str(result)
