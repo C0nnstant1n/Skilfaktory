5d58df47a8bd97c578e0b0788e6cda88abaa7345
C0nstant1n <connstantino@mail.ru>
2023-02-01 10:10:56 +0700

    Почистили папку от мусора

--- who_over.py
+++
@@ -1,7 +0,0 @@
-import sqr_circ
-
-r = int(input("Enter radius: "))
-a = int(input("Enter side 'a' of rectangle: "))
-b = int(input("Enter side 'b' of rectangle: "))
-
-print("circle") if sqr_circ.circle_sqr(r) > sqr_circ.rect_sqr(a, b) else print("square")
