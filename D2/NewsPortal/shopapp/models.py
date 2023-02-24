aeb332a5f907090987519fb384f45f74d6bd2314
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-02-24 23:52:29 +0700

    Начал писать проект NewsPortal, создал модели

--- D2/NewsPortal/shopapp/models.py
+++ D2/NewsPortal/shopapp/models.py
@@ -1,27 +1,30 @@
 from django.db import models
-
-
-director = 'DI'
-admin = 'AD'
-cook = 'CO'
-cashier = 'CA'
-cleaner = 'CL'
-
-POSITIONS = [
-    (director, 'Директор'),
-    (admin, 'Администратор'),
-    (cook, 'Повар'),
-    (cashier, 'Кассир'),
-    (cleaner, 'Уборщик')
-]
+from datetime import datetime
 
 
 class Staff(models.Model):
+    director = 'DI'
+    admin = 'AD'
+    cook = 'CO'
+    cashier = 'CA'
+    cleaner = 'CL'
+
+    POSITIONS = [
+        (director, 'Директор'),
+        (admin, 'Администратор'),
+        (cook, 'Повар'),
+        (cashier, 'Кассир'),
+        (cleaner, 'Уборщик')
+    ]
+
     full_name = models.CharField(max_length=100)
-    models.CharField(max_length=2,
-                     choices=POSITIONS,
-                     default=cashier)
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS,
+                                default=cashier)
     labor_contract = models.IntegerField()
+
+    def last_name(self):
+        return self.full_name.split()[0]
 
 
 class Product(models.Model):
@@ -39,8 +42,31 @@ class Order(models.Model):
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='ProductOrder')
 
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:
+            return (datetime.now() - self.time_in).total_seconds() // 60
+
 
 class ProductOrder(models.Model):
-    amount = models.IntegerField(default=1)
+    _amount = models.IntegerField(default=1, db_column='amount')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def product_sum(self):
+        return self.product.price * self.amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
--- D2/NewsPortal/shopapp/views.py
+++ D2/NewsPortal/shopapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from models import Product
+
+keyboard = Product.objects.create(name='Клавиатура', price=1060)
+utf_cable = Product(name='витая пара (3 м.)', price=993)
+utf_cable.save()
