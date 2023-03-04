3fd802ded51fc3afbc6ea300b5f20b2e640d66f7
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-03-04 13:27:12 +0700

    выполнил задания д3.3 по проекту News Portal

--- D2/NewsPortal/newsapp/views.py
+++ D2/NewsPortal/newsapp/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -8,10 +8,16 @@ class PostsList(ListView):
     # Указываем модель, объекты которой мы будем выводить
     model = Post
     # Поле, которое будет использоваться для сортировки объектов
-    ordering = 'id'
+    ordering = '-post_date'
     # Указываем имя шаблона, в котором будут все инструкции о том,
     # как именно пользователю должны быть показаны наши объекты
     template_name = 'posts.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
