1ce5d3a146cb540b9849e7ac32546bcc8f5f5987
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-03-04 18:18:28 +0700

    изучаем фильтры и теги d3.4

---
+++ D1/djangoFlat/simpleapp/templatetags/custom_filters.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+CURRENCY_SYMBOLS = {
+    'rub': 'р',
+    'usd': '$'
+}
+
+
+@register.filter()
+def currency(value, code='rub'):
+    postfix = CURRENCY_SYMBOLS[code]
+    return f"{value} {postfix} р."
