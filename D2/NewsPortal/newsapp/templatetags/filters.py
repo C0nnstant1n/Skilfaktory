7210e9a81969496e398c9f83e77fec388e29bf6b
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-03-04 21:33:28 +0700

    добавил проверку аргумента на str в фильтре censor

--- D2/NewsPortal/newsapp/templatetags/filters.py
+++ D2/NewsPortal/newsapp/templatetags/filters.py
@@ -6,8 +6,11 @@ BAD_WORDS = ('Редиск', 'редиск', 'Сука', 'сука', 'блядь
 
 
 @register.filter()
-# Проверяем встречаются ли в тексте слова из БЭД кортежа
+# censor проверяет, встречаются ли в тексте слова из БЭД кортежа
 def censor(text):
+    # проверяем является ли text строкой
+    if type(text) is not str:
+        raise TypeError(f"фильтр не может принять  {type(text)}, только <str>")
     for word in BAD_WORDS:
         if word in text:
             # подменяем плохие слова на *
