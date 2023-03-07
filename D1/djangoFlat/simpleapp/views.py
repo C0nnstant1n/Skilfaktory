06c76aa58ea40e5fedcfc5f5ad11fa2f303f080f
Konstantin <c0nnstant1n.cs@gmail.com>
2023-03-07 18:50:22 +0700

    studying D4.2

--- D1/djangoFlat/simpleapp/views.py
+++ D1/djangoFlat/simpleapp/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Product
+from .filters import ProductFilter
 
 
 class ProductsList(ListView):
@@ -16,10 +17,25 @@ class ProductsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+    paginate_by = 3  # вот так мы можем указать количество записей на странице
+
+    # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
+        context['filterset'] = self.filterset
         context['next_sale'] = None
         return context
 
