55e0a31b409150daf5170c95da833c51572a1483
Konstantin <c0nnstant1n.cs@gmail.com>
2023-03-10 18:23:30 +0700

    add pagination, begin add search

--- D2/NewsPortal/newsapp/urls.py
+++ D2/NewsPortal/newsapp/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import PostsList, PostDetail
+from .views import PostsList, PostDetail, Search
+from .filters import PostFilter
 
 
 urlpatterns = [
    # path — означает путь.
-   # В данном случае путь ко всем товарам у нас останется пустым,
-   # чуть позже станет ясно почему.
+   # В данном случае путь ко всем постам у нас останется пустым,
    # Т.к. наше объявленное представление является классом,
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
-   path('', PostsList.as_view()),
-   path('<int:pk>', PostDetail.as_view()),
+   path('', PostsList.as_view(), name='posts'),
+   path('<int:pk>', PostDetail.as_view(), name='post'),
+   path('search', Search.as_view(), name='search'),
 ]
--- D2/NewsPortal/newsapp/views.py
+++ D2/NewsPortal/newsapp/views.py
@@ -2,6 +2,7 @@
 
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
 
 
 class PostsList(ListView):
@@ -15,9 +16,25 @@ class PostsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
+
+
+class Search(PostsList):
+    template_name = 'search.html'
+
