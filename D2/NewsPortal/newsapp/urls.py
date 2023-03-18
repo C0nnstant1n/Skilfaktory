b9eaf85ee0f2b7981e8c53b56c47b735659fd03e
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-03-18 22:49:35 +0700

    Auto stash before merge of "main" and "origin/main"

--- D2/NewsPortal/newsapp/urls.py
+++ D2/NewsPortal/newsapp/urls.py
@@ -10,14 +10,15 @@ urlpatterns = [
    # Т.к. наше объявленное представление является классом,
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
+   # path('', home),
    path('news/', PostsList.as_view(), name='posts'),
    path('news/<int:pk>', PostDetail.as_view(), name='post'),
    path('news/search', Search.as_view(), name='search'),
    path('news/create', CreatePost.as_view(),  name='create_news'),
    path('news/<int:pk>/edit/', EditPost.as_view(), name='edit_news'),
    path('news/<int:pk>/delete/', DeletePost.as_view(), name='delete'),
-   path('articles/create', CreateArticle.as_view(),  name='create_news'),
-   path('articles/<int:pk>/edit/', EditArticle.as_view(), name='edit_news'),
+   path('articles/create', CreateArticle.as_view(),  name='create_articles'),
+   path('articles/<int:pk>/edit/', EditArticle.as_view(), name='edit_articles'),
    path('articles/<int:pk>/delete/', DeletePost.as_view(), name='delete'),
 
 
