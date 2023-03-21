16b6df6948faa3b02b771e529046596731b8788b
Konstantin <c0nnstant1n.cs@gmail.com>
2023-03-21 12:50:35 +0700

    change Comment.post

--- D2/NewsPortal/newsapp/models.py
+++ D2/NewsPortal/newsapp/models.py
@@ -80,7 +80,7 @@ class Comment(models.Model):
     time = models.DateTimeField(auto_now_add=True)
     text_comment = models.CharField(max_length=255)
     rate_comment = models.IntegerField(default=0)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="posts")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user_comment = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
