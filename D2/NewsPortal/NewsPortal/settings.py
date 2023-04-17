0d4a4133c9e5103d74833663448e80aa315fbb38
Konstantin <c0nnstant1n.cs@gmail.com>
2023-04-17 18:31:32 +0700

    add some secutity, minor changes in templates

--- D2/NewsPortal/NewsPortal/settings.py
+++ D2/NewsPortal/NewsPortal/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
