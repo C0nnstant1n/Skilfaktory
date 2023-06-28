b442735f777a22fbc329b4a0390c54cae9b0d16a
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-06-28 18:46:17 +0700

    Исправляем различия

---
+++ MyHobby/Script/8.js
@@ -0,0 +1,13 @@
+let fruits = new Map([
+
+    ["apple", "green"],
+
+    ["strawberry", "red"],
+
+    ["blueberry",    "blue"]
+
+]);
+
+for (let key of fruits.keys()){
+    console.log(`Key - ${key}, Value - ${fruits.get(key)}`)
+}
