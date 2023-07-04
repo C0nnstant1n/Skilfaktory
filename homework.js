24bae41891ae172dc8c96d41072408f8ddd644fd
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-07-04 17:04:33 +0700

    добавил коментарии

--- homework.js
+++ homework.js
@@ -1,20 +1,23 @@
+// класс родитель электроприборы
 class ElectricalApp{
     constructor(power) {
-        this.power = power;
-        this.isOn = false;
-    }
-    setOn(){
+        this.power = power; // свойство - мощность
+        this.isOn = false;  // свойство - включен ли прибор
+        }
+    setOn(){          // метод родителя включить прибор
         this.isOn = true;
     }
-    setOff(){
+    setOff(){         // метод родителя выключить прибор
         this.isOn = false;
     }
 }
 
+// класс потомок электролампа
 class Lamp extends ElectricalApp{
     constructor(power, type) {
         super(power);
-        this.type = type;
+        this.type = type;   // свойство потомка определяет свойство присущее этому потомку
+                            // например электролампа может быть светодиодной или лампой накаливания
     }
 }
 
@@ -32,10 +35,10 @@ class Kettle extends ElectricalApp{
     }
 }
 
-lamp1 = new Lamp(15, 'led');
-lamp1.setOff();
-lamp2 = new Lamp(65, 'filament');
-lamp2.setOn();
+lamp1 = new Lamp(15, 'led');        // создаем экземпляр лампы
+lamp1.setOff();                                 // выключаем лампу
+lamp2 = new Lamp(65, 'filament');   // создаем экземпляр другой лампы
+lamp2.setOn();                                  // включаем лампу
 comp = new Comp(450, 'PC');
 comp.setOn();
 kettle = new Kettle(2000, 'thermopot');
@@ -44,7 +47,7 @@ let equip = [lamp1, lamp2, comp, kettle];
 let sumOnPower = 0;
 for (let key in equip){
     if (equip[key].isOn){
-        sumOnPower += equip[key].power
+        sumOnPower += equip[key].power          // подсчитываем суммарную мощность включенных приборов
     }
 }
 console.log(`Мощность включенных приборов - ${sumOnPower} Ватт`)
