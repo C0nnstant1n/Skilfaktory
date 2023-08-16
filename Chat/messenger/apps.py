8e2157c5991b2aae7da7e9e967daf229e4050ce3
C0nstant1n <connstantino@mail.ru>
2023-08-16 16:06:01 +0700

    Clear main branch

--- Chat/messenger/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MessendgerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messenger'
-
-    def ready(self):
-        from . import signals
