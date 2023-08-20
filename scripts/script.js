f494337f28a74727d63ce4fa85231ec867a0ef4e
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-08-20 23:57:39 +0700

    Add timeline, rewright some styles

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,39 @@ const coll = document.getElementsByClassName("collapsible");
 let i, n;
 const collapse = document.getElementsByClassName("content")
 
-for (n of collapse){
-  n.style.display = "none"
+console.log(window.screen.availWidth)
+
+for (n of collapse) {
+    n.style.display = "none"
 }
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-      const content = this.nextElementSibling;
-      if (content.style.display === "grid") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "grid";
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "grid") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "grid";
+        }
+    });
+}
+
+let popup = document.getElementById('photo-popup')
+popup_toggle = document.getElementById('logo-btn')
+popup_close = document.querySelector('.close')
+popup_content = document.getElementById('popup-content')
+
+popup_toggle.onclick = function () {
+    popup.style.display = "flex"
+}
+
+popup_close.onclick = function () {
+    popup.style.display = "none"
+}
+
+window.onclick = function (e) {
+    if (e.target == popup_content) {
+        popup.style.display = "none"
     }
-  });
 }
